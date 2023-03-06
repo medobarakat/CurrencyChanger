2ada45b9dd8743b7973a553cd3f309f1fe83c285
medobarakat <ahmedbarakat2401@gmail.com>
2023-03-06 02:26:59 -0800

    currency worked

--- src/App.js
+++ src/App.js
@@ -1,19 +1,93 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactFlagsSelect from "react-flags-select";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 
 import "./App.css";
 import { Col, Container, Image, Row } from "react-bootstrap";
+import axios from "axios";
 function App() {
   const [selected, setSelected] = useState("");
-  const [selected1, setSelected1] = useState("");
-  const [value, setValue] = useState(10);
+  const [fromValue,setFromValue] = useState("");
 
+  const [selected1, setSelected1] = useState("");
+  const [toValue,setToValue] = useState("");
+
+  const [value, setValue] = useState(10);
+  const [result, setResult] = useState();
+  
+
+  const change1 = (e)=>{
+    if(e === "US"){
+      setFromValue("usd")
+      setSelected("US")
+    }else if(e === "GB"){
+      setFromValue("gbp")
+      setSelected("GB")
+
+    }else if(e === "JP"){
+      setFromValue("JPY")
+      setSelected("JP")
+
+    }else if(e=== "SA"){
+      setFromValue("sar")
+      setSelected("SA")
+
+    }else if(e=== "EG"){
+      setFromValue("egp")
+      setSelected("EG")
+
+    }
+  }
+
+  const change2 = (e)=>{
+    if(e === "US"){
+      setToValue("usd")
+      setSelected1("US")
+    }else if(e === "GB"){
+      setToValue("gbp")
+      setSelected1("GB")
+    }else if(e === "JP"){
+      setToValue("JPY")
+      setSelected1("JP")
+    }else if(e=== "SA"){
+      setToValue("sar")
+      setSelected1("SA")
+    }else if(e=== "EG"){
+      setToValue("egp")
+      setSelected1("EG")
+    }
+  }
+
+  const converter = () => {
+    var myHeaders = new Headers();
+myHeaders.append("apikey", "idTf5aNO1ONgML4LE1Nz8gdT03mFDUEb");
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch(`https://api.apilayer.com/fixer/convert?to=${toValue}&from=${fromValue}&amount=${value}`, requestOptions)
+  .then(response => response.text())
+  .then(result => 
+{
+ var obj = JSON.parse(result)
+  setResult(obj.result)
+}
+    
+    )
+  .catch(error => console.log('error', error));
+
+  };
+  useEffect(() => {
+    converter();
+  }, [value]);
   return (
     <Container>
-      <Row style={{marginLeft:"4rem"}}>
-        <Col md={5} xs={5} style={{ display: "flex", marginTop: "5rem" }}>
+      <Row style={{ marginLeft: "4rem" }}>
+        <Col md={5} sm={5} style={{ display: "flex", marginTop: "5rem" }}>
           <div className="singlepick">
             <ReactFlagsSelect
               countries={["US", "GB", "JP", "SA", "EG"]}
@@ -26,7 +100,7 @@ function App() {
               }}
               placeholder="Select Language"
               selected={selected}
-              onSelect={(code) => setSelected(code)}
+              onSelect={(code) => change1(code)}
             />
           </div>
           <div className="singlepick">
@@ -41,17 +115,18 @@ function App() {
               }}
               placeholder="Select Language"
               selected={selected1}
-              onSelect={(code) => setSelected1(code)}
+              onSelect={(code) => change2(code)}
             />
           </div>
-         
         </Col>
-        <Col md={7} xs={9} style={{ display: "flex", marginTop: "5rem" }}>
-        <div>
+        <Col md={6} sm={9} style={{ display: "flex", marginTop: "5rem" }}>
+          <div>
             <Form.Control
               className="numpick"
               type="number"
               placeholder="Enter Ampunt"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
             />
           </div>
           <div
@@ -72,11 +147,11 @@ function App() {
               marginRight: "1rem",
             }}
           >
-            {value}
+            {result}
           </div>
         </Col>
       </Row>
-      <Row style={{marginTop:"4rem" , marginLeft:"4rem"}}>
+      <Row style={{ marginTop: "4rem", marginLeft: "4rem" }}>
         <Col>1 of 3</Col>
         <Col>2 of 3</Col>
       </Row>
